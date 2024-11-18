fb7e4ccf6aecf2be5dd8423cba128315f63285ee
Babajan <babajan.patan@wealthy.in>
2024-11-18 20:12:20 +0530

    added analytics

--- src/App.js
+++ src/App.js
@@ -5,6 +5,16 @@ import UsageMaps from './Components/UsageMaps';
 function App() {
   return (
     <div className="App">
+     <head>
+      <title>Usage Maps Application</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta name="description" content="A powerful tool for visualizing usage maps and statistics." />
+      <link rel="icon" href="/favicon.ico" />
+      <script>
+      window.va = window.va || function () { (window.vaq = window.vaq || []).push(arguments) };
+    </script>
+    <script defer src="/_vercel/insights/script.js"></script>
+    </head>
     <UsageMaps />
     </div>
   );
